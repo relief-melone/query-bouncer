7811c66a674754c12f56fdfdd9d5170fc08aa9eb
xan2063 <alex.max.stark@gmail.com>
2020-03-24 23:31:49 +0100

    fix null reference if no user found

--- src/controllers/controller.populatedPermissions.getForCurrentUser.ts
+++ src/controllers/controller.populatedPermissions.getForCurrentUser.ts
@@ -5,10 +5,10 @@ import Permission from '../interfaces/interface.Permission';
 
 export default(  getPopulatedPermissionForCurrentUser: (user: string|null) => Promise<Permission[]>, errorHandler = ErrorHandler)=>
   async ( req: Request, res: Response ): Promise<Response> => {
-
-    const user = (req as any).user[configMain.userPrimaryKey];
+    const user = (req as any).user;
+    const username = user? user[configMain.userPrimaryKey] :null;
     try {
-      const permissions = await getPopulatedPermissionForCurrentUser(user);
+      const permissions = await getPopulatedPermissionForCurrentUser(username);
       return res.status(200).json(permissions);
     } catch (err) {
       return errorHandler(err, res);
