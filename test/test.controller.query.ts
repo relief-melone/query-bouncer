302b1e08b20873d1d19ea316ebfa28c30e019daf
Relief Melone <relief.melone@gmail.com>
2020-10-30 12:17:32 +0100

    added tests for user=null

--- test/test.controller.query.ts
+++ test/test.controller.query.ts
@@ -1,6 +1,6 @@
-import { QueryController } from '../src/controllers/controller.query';
 import sinon from 'sinon';
 import mainConfig from '../src/configs/config.main';
+import { QueryController } from '../src/controllers/controller.query';
 import errorFactory from '../src/services/error/service.errors';
 
 describe('controller.query', () => {
@@ -53,6 +53,54 @@ describe('controller.query', () => {
     };
     const getRoleAssignmentsForUser = sinon.stub().returns(Promise.resolve([
       { User: 'someUser',
+        Role: 'admin',
+        Data: { someData: 'myVal',
+          someOtherData: 'moreData' } }
+    ]));
+    const getRoleByTitle = sinon.stub().returns(Promise.resolve({
+      Title: 'admin',
+      Permissions: ['someTitle1', 'someTitle2']
+    }));
+    const getPermissions = sinon.stub().returns(Promise.resolve([
+      { Title: 'someTitle1',
+        Collection: 'myCollection',
+        Right: 'read',
+        ExcludedPaths: [],
+        QueryRestriction: { SpecialVal: '${someData}' } },
+      { Title: 'someTitle2',
+        Collection: 'myCollection',
+        Right: 'read',
+        ExcludedPaths: [],
+        QueryRestriction: { NewVal: '${someOtherData}' } }
+    ]));
+
+    // Execute
+    await QueryController(req, res, next, mainConfig, getRoleAssignmentsForUser, getRoleByTitle, getPermissions );
+
+    // Assert
+    sinon.assert.calledOnce(res.status);
+    sinon.assert.calledOnce(res.json);
+    sinon.assert.calledWith(res.status,200);
+    sinon.assert.calledWith(res.json, {
+      query: { $or: [{ SpecialVal: 'myVal' }, { NewVal: 'moreData' }] }
+    });
+    sinon.assert.calledWith(res.json, {
+      query: {
+        $or : [{ SpecialVal: 'myVal' }, { NewVal: 'moreData' }]
+      }      
+    });
+  });
+
+  it('will correctly return the populated permission queries with valid input if user was null', async () => {
+    // Prepare
+    const req: any = {
+      user : null,
+      body : { query: {} },
+      params: { Right: 'read',
+        Collection: 'myCollection' }
+    };
+    const getRoleAssignmentsForUser = sinon.stub().returns(Promise.resolve([
+      { User: '$anyone',
         Role: 'admin',
         Data: { someData: 'myVal',
           someOtherData: 'moreData' } }
